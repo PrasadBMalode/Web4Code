148ded9daec015eeb1b756bf83913ba9dee5b1b9
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-11-12 21:41:13 +0530

    Data types in JS

---
+++ JS/datatype.js
@@ -0,0 +1,23 @@
+var a=6360211265;
+console.log(a);
+console.log(typeof a);
+
+var b=637836n;
+console.log(b);
+console.log(typeof b);
+
+var c=true;
+console.log(c);
+console.log(typeof c);
+
+var d="Prasad";
+console.log(d);
+console.log(typeof d);
+
+var e;
+console.log(typeof e);
+
+var f=Symbol("@@@@!!!");
+console.log(f);
+console.log(typeof f);
+
