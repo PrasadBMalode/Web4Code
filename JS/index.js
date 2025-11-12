a1e23e494a98f8368de5b852a64650967391790b
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-11-12 17:01:27 +0530

    Both DataTypes and Hoisting

---
+++ JS/index.js
@@ -0,0 +1,76 @@
+console.log("Iam prasad BM  \n iam doing my internship at xworkz as full stack develpment \n and iam fro RANEBENNUR")
+var phNumber=6360211265;
+console.log("phone number==",phNumber);
+
+var name="PRASAD";
+console.log("Student name===",name);
+
+var id;
+console.log(id);
+
+var cllg="Akash";
+ cllg="RTES";
+console.log("cllg name===",cllg);
+
+
+console.log("DATA TYPES.........");
+ var a=123456789;
+ console.log(typeof a);
+
+ var b=456894735n;
+ console.log(typeof b);
+
+ var c=true;
+ console.log(typeof c);
+
+ var d="Sudeep";
+ console.log(typeof d);
+
+  var h;
+ console.log(typeof h);
+
+  var g=Symbol("y56u");
+ console.log(typeof g);
+
+ var e=[1, 5, "prasad", true, 33];
+ console.log(e);
+ console.log(typeof e);
+
+ var f={name:"Prasad", age:22};
+ console.log(f);
+ console.log(typeof f);
+
+ function mssg(){
+    console.log("first mssg");
+ }
+
+ console.log("second message");
+ mssg();
+ console.log(typeof mssg);
+
+ function data(){
+    console.log("this is data belongs to prasad")
+ }
+
+ data();
+
+ console.log("-----------------------");
+ console.log("Hoisting.......");
+ //declaring a variable above the scope of accesing is called Hoisting.
+
+console.log(s);
+ var s="Shop";
+
+
+ id();
+ function id(){
+    console.log("101..........");
+ }
+
+ 
+
+
+ 
+
+
+
