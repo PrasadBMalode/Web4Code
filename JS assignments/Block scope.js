f6bb7f974d606a5eb4601aac4a73bddfe048992a
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-11-14 11:02:56 +0530

    function and block scope, arrow function

---
+++ JS assignments/Block scope.js
@@ -0,0 +1,17 @@
+let price=200;
+function a(){
+    if(true){
+        let price=400;
+    }
+    console.log(price);
+}
+a();
+
+let amount=100;
+function b(){
+    if(true){
+        let cost=30;
+    }
+    console.log(cost);
+}
+b();
---
+++ JS assignments/function Scope.js
@@ -0,0 +1,18 @@
+var price=100;
+function p(){
+    if(true){
+        var price=200;
+    }
+    console.log(price);
+}
+p();
+
+var amount=300;
+function a(){
+    if(true){
+        var cost=400;
+        console.log(amount);
+    }
+    console.log(cost);
+}
+a();
