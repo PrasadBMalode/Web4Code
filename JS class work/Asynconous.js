eeccc617dbf94d28abf8baa80a43dd17e8caa757
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-11-20 17:38:39 +0530

    Dom and Asyncronus

--- JS class work/Asynconous.js
+++ JS class work/Asynconous.js
@@ -4,6 +4,10 @@ setTimeout(function (){
     console.log("1st set timout......");
 },5000);
 
+for(i=0; i<10; i++){
+    console.log(i);
+}
+
 setTimeout(function (){
     console.log("2nd  set timeout......")
 },2000);
---
+++ JS class work/DOM.js
@@ -0,0 +1,13 @@
+  var value=document.children[0].children[1].children[0];
+
+ console.log(value);
+ console.log(value.innerHTML);
+
+ value.innerText="Manipulating through JS.....";
+
+ value.innerHTML="<b style='color:red'> inner HTML......</b>";
+
+
+var a=document.getElementById("h1");
+console.log(a.innerText);
+
